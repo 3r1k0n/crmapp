6d6d0a6fb5666dea60576838fc7e303d2b768662
Erik Percic <3r1k0n@gmail.com>
2019-12-08 19:31:39 -0500

    Add Bootstrap bundle and custom css to layout

--- config/web.php
+++ config/web.php
@@ -3,6 +3,29 @@ return [
     'id' => 'crmapp',
     'basePath' => realpath(__DIR__ . '/../'),
     'components' => [
+        'assetManager' => [
+            'converter' => [
+                'class' => 'yii\web\AssetConverter',
+                'commands' => [
+                    'less' => ['css', 'lessc {from} {to} --no-color'],
+                ],
+            ],
+            'bundles' => [
+                'yii\bootstrap\BootstrapAsset' => [
+                    'sourcePath' => '@app/assets/source/bootstrap',
+                    'css' => [
+                        'css/bootstrap.less'
+                    ],
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'sourcePath' => '@app/assets/source/bootstrap',
+                ],
+                'yii\bootstrap\BootstrapThemeAsset' => [
+                    'sourcePath' => '@app/assets/source/bootstrap',
+                ],
+            ],
+            'linkAssets' => false,
+        ],
         'request' => [
             'cookieValidationKey' => '1021c636f4ae69fb3eb71c66441c9458',
         ],
@@ -11,5 +34,5 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false
         ]
-    ]
+    ],
 ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,22 @@
+<?php
+    use app\assets\AppAsset;
+    AppAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?= Yii::$app->language ?>">
 <head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRM</title>
+    <?php $this->head() ?>
 </head>
 <body>
-<?= $content ?>
+    <?php $this->beginBody() ?>
+    <?= $content ?>
+    <?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
